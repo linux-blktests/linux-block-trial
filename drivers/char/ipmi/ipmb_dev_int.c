kawasaki/linux-block

char/ipmi: convert to read/write iterators
Signed-off-by: Jens Axboe <axboe@kernel.dk>

drivers/char/ipmi/ipmb_dev_int.c
@@ -71,11 +71,11 @@ static inline struct ipmb_dev *to_ipmb_dev(struct file *file)
 	return container_of(file->private_data, struct ipmb_dev, miscdev);
 }
 
-static ssize_t ipmb_read(struct file *file, char __user *buf, size_t count,
-			loff_t *ppos)
+static ssize_t ipmb_read(struct kiocb *iocb, struct iov_iter *to)
 {
-	struct ipmb_dev *ipmb_dev = to_ipmb_dev(file);
+	struct ipmb_dev *ipmb_dev = to_ipmb_dev(iocb->ki_filp);
 	struct ipmb_request_elem *queue_elem;
+	size_t count = iov_iter_count(to);
 	struct ipmb_msg msg;
 	ssize_t ret = 0;
 
@@ -86,7 +86,7 @@ static ssize_t ipmb_read(struct file *file, char __user *buf, size_t count,
 	while (list_empty(&ipmb_dev->request_queue)) {
 		spin_unlock_irq(&ipmb_dev->lock);
 
-		if (file->f_flags & O_NONBLOCK)
+		if (iocb->ki_filp->f_flags & O_NONBLOCK)
 			return -EAGAIN;
 
 		ret = wait_event_interruptible(ipmb_dev->wait_queue,
@@ -107,7 +107,7 @@ static ssize_t ipmb_read(struct file *file, char __user *buf, size_t count,
 	spin_unlock_irq(&ipmb_dev->lock);
 
 	count = min_t(size_t, count, msg.len + 1);
-	if (copy_to_user(buf, &msg, count))
+	if (!copy_to_iter_full(&msg, count, to))
 		ret = -EFAULT;
 
 	return ret < 0 ? ret : count;
@@ -176,6 +176,7 @@ static ssize_t ipmb_write(struct file *file, const char __user *buf,
 
 	return ret < 0 ? ret : count;
 }
+FOPS_WRITE_ITER_HELPER(ipmb_write);
 
 static __poll_t ipmb_poll(struct file *file, poll_table *wait)
 {
@@ -193,10 +194,10 @@ static __poll_t ipmb_poll(struct file *file, poll_table *wait)
 }
 
 static const struct file_operations ipmb_fops = {
-	.owner	= THIS_MODULE,
-	.read	= ipmb_read,
-	.write	= ipmb_write,
-	.poll	= ipmb_poll,
+	.owner		= THIS_MODULE,
+	.read_iter	= ipmb_read,
+	.write_iter	= ipmb_write_iter,
+	.poll		= ipmb_poll,
 };
 
 /* Called with ipmb_dev->lock held. */

drivers/char/ipmi/ipmi_watchdog.c
@@ -37,6 +37,7 @@
 #include <linux/delay.h>
 #include <linux/atomic.h>
 #include <linux/sched/signal.h>
+#include <linux/uio.h>
 
 #ifdef CONFIG_X86
 /*
@@ -777,12 +778,11 @@ static ssize_t ipmi_write(struct file *file,
 	}
 	return len;
 }
+FOPS_WRITE_ITER_HELPER(ipmi_write);
 
-static ssize_t ipmi_read(struct file *file,
-			 char        __user *buf,
-			 size_t      count,
-			 loff_t      *ppos)
+static ssize_t ipmi_read(struct kiocb *iocb, struct iov_iter *to)
 {
+	size_t count = iov_iter_count(to);
 	int          rv = 0;
 	wait_queue_entry_t wait;
 
@@ -795,7 +795,7 @@ static ssize_t ipmi_read(struct file *file,
 	 */
 	spin_lock_irq(&ipmi_read_lock);
 	if (!data_to_read) {
-		if (file->f_flags & O_NONBLOCK) {
+		if (iocb->ki_filp->f_flags & O_NONBLOCK) {
 			rv = -EAGAIN;
 			goto out;
 		}
@@ -821,7 +821,7 @@ static ssize_t ipmi_read(struct file *file,
 	spin_unlock_irq(&ipmi_read_lock);
 
 	if (rv == 0) {
-		if (copy_to_user(buf, &data_to_read, 1))
+		if (!copy_to_iter_full(&data_to_read, 1, to))
 			rv = -EFAULT;
 		else
 			rv = 1;
@@ -895,9 +895,9 @@ static int ipmi_close(struct inode *ino, struct file *filep)
 
 static const struct file_operations ipmi_wdog_fops = {
 	.owner   = THIS_MODULE,
-	.read    = ipmi_read,
+	.read_iter    = ipmi_read,
 	.poll    = ipmi_poll,
-	.write   = ipmi_write,
+	.write_iter   = ipmi_write_iter,
 	.unlocked_ioctl = ipmi_unlocked_ioctl,
 	.compat_ioctl	= compat_ptr_ioctl,
 	.open    = ipmi_open,

linux-blktests/linux-block-trial

drivers/char/ipmi/ipmb_dev_int.c
@@ -71,11 +71,11 @@ static inline struct ipmb_dev *to_ipmb_dev(struct file *file)
 	return container_of(file->private_data, struct ipmb_dev, miscdev);
 }
 
-static ssize_t ipmb_read(struct file *file, char __user *buf, size_t count,
-			loff_t *ppos)
+static ssize_t ipmb_read(struct kiocb *iocb, struct iov_iter *to)
 {
-	struct ipmb_dev *ipmb_dev = to_ipmb_dev(file);
+	struct ipmb_dev *ipmb_dev = to_ipmb_dev(iocb->ki_filp);
 	struct ipmb_request_elem *queue_elem;
+	size_t count = iov_iter_count(to);
 	struct ipmb_msg msg;
 	ssize_t ret = 0;
 
@@ -86,7 +86,7 @@ static ssize_t ipmb_read(struct file *file, char __user *buf, size_t count,
 	while (list_empty(&ipmb_dev->request_queue)) {
 		spin_unlock_irq(&ipmb_dev->lock);
 
-		if (file->f_flags & O_NONBLOCK)
+		if (iocb->ki_filp->f_flags & O_NONBLOCK)
 			return -EAGAIN;
 
 		ret = wait_event_interruptible(ipmb_dev->wait_queue,
@@ -107,7 +107,7 @@ static ssize_t ipmb_read(struct file *file, char __user *buf, size_t count,
 	spin_unlock_irq(&ipmb_dev->lock);
 
 	count = min_t(size_t, count, msg.len + 1);
-	if (copy_to_user(buf, &msg, count))
+	if (!copy_to_iter_full(&msg, count, to))
 		ret = -EFAULT;
 
 	return ret < 0 ? ret : count;
@@ -132,10 +132,10 @@ static int ipmb_i2c_write(struct i2c_client *client, u8 *msg, u8 addr)
 	return i2c_transfer(client->adapter, &i2c_msg, 1);
 }
 
-static ssize_t ipmb_write(struct file *file, const char __user *buf,
-			size_t count, loff_t *ppos)
+static ssize_t ipmb_write(struct kiocb *iocb, struct iov_iter *from)
 {
-	struct ipmb_dev *ipmb_dev = to_ipmb_dev(file);
+	struct ipmb_dev *ipmb_dev = to_ipmb_dev(iocb->ki_filp);
+	size_t count = iov_iter_count(from);
 	u8 rq_sa, netf_rq_lun, msg_len;
 	struct i2c_client *temp_client;
 	u8 msg[MAX_MSG_LEN];
@@ -144,7 +144,7 @@ static ssize_t ipmb_write(struct file *file, const char __user *buf,
 	if (count > sizeof(msg))
 		return -EINVAL;
 
-	if (copy_from_user(&msg, buf, count))
+	if (!copy_from_iter_full(&msg, count, from))
 		return -EFAULT;
 
 	if (count < msg[0])
@@ -193,10 +193,10 @@ static __poll_t ipmb_poll(struct file *file, poll_table *wait)
 }
 
 static const struct file_operations ipmb_fops = {
-	.owner	= THIS_MODULE,
-	.read	= ipmb_read,
-	.write	= ipmb_write,
-	.poll	= ipmb_poll,
+	.owner		= THIS_MODULE,
+	.read_iter	= ipmb_read,
+	.write_iter	= ipmb_write,
+	.poll		= ipmb_poll,
 };
 
 /* Called with ipmb_dev->lock held. */

drivers/char/ipmi/ipmi_watchdog.c
@@ -37,6 +37,7 @@
 #include <linux/delay.h>
 #include <linux/atomic.h>
 #include <linux/sched/signal.h>
+#include <linux/uio.h>
 
 #ifdef CONFIG_X86
 /*
@@ -710,12 +711,10 @@ static long ipmi_unlocked_ioctl(struct file *file,
 	return ret;
 }
 
-static ssize_t ipmi_write(struct file *file,
-			  const char  __user *buf,
-			  size_t      len,
-			  loff_t      *ppos)
+static ssize_t ipmi_write(struct kiocb *iocb, struct iov_iter *from)
 {
 	int rv;
+	size_t len = iov_iter_count(from);
 
 	if (len) {
 		if (!nowayout) {
@@ -727,7 +726,7 @@ static ssize_t ipmi_write(struct file *file,
 			for (i = 0; i != len; i++) {
 				char c;
 
-				if (get_user(c, buf + i))
+				if (get_iter(c, from))
 					return -EFAULT;
 				if (c == 'V')
 					expect_close = 42;
@@ -740,11 +739,9 @@ static ssize_t ipmi_write(struct file *file,
 	return len;
 }
 
-static ssize_t ipmi_read(struct file *file,
-			 char        __user *buf,
-			 size_t      count,
-			 loff_t      *ppos)
+static ssize_t ipmi_read(struct kiocb *iocb, struct iov_iter *to)
 {
+	size_t count = iov_iter_count(to);
 	int          rv = 0;
 	wait_queue_entry_t wait;
 
@@ -757,7 +754,7 @@ static ssize_t ipmi_read(struct file *file,
 	 */
 	mutex_lock(&ipmi_read_mutex);
 	if (!data_to_read) {
-		if (file->f_flags & O_NONBLOCK) {
+		if (iocb->ki_filp->f_flags & O_NONBLOCK) {
 			rv = -EAGAIN;
 			goto out;
 		}
@@ -783,7 +780,7 @@ static ssize_t ipmi_read(struct file *file,
 	mutex_unlock(&ipmi_read_mutex);
 
 	if (rv == 0) {
-		if (copy_to_user(buf, &data_to_read, 1))
+		if (!copy_to_iter_full(&data_to_read, 1, to))
 			rv = -EFAULT;
 		else
 			rv = 1;
@@ -857,9 +854,9 @@ static int ipmi_close(struct inode *ino, struct file *filep)
 
 static const struct file_operations ipmi_wdog_fops = {
 	.owner   = THIS_MODULE,
-	.read    = ipmi_read,
+	.read_iter    = ipmi_read,
 	.poll    = ipmi_poll,
-	.write   = ipmi_write,
+	.write_iter   = ipmi_write,
 	.unlocked_ioctl = ipmi_unlocked_ioctl,
 	.compat_ioctl	= compat_ptr_ioctl,
 	.open    = ipmi_open,