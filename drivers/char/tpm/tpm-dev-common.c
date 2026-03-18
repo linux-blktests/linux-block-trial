kawasaki/linux-block

char/tpm: convert to read/write iterators
Signed-off-by: Jens Axboe <axboe@kernel.dk>

drivers/char/tpm/tpm-dev-common.c
@@ -124,10 +124,10 @@ void tpm_common_open(struct file *file, struct tpm_chip *chip,
 	file->private_data = priv;
 }
 
-ssize_t tpm_common_read(struct file *file, char __user *buf,
-			size_t size, loff_t *off)
+ssize_t tpm_common_read_iter(struct kiocb *iocb, struct iov_iter *to)
 {
-	struct file_priv *priv = file->private_data;
+	struct file_priv *priv = iocb->ki_filp->private_data;
+	size_t size = iov_iter_count(to);
 	ssize_t ret_size = 0;
 	int rc;
 
@@ -142,21 +142,21 @@ ssize_t tpm_common_read(struct file *file, char __user *buf,
 			goto out;
 		}
 
-		rc = copy_to_user(buf, priv->data_buffer + *off, ret_size);
+		rc = !copy_to_iter_full(priv->data_buffer + iocb->ki_pos, ret_size, to);
 		if (rc) {
 			memset(priv->data_buffer, 0, TPM_BUFSIZE);
 			priv->response_length = 0;
 			ret_size = -EFAULT;
 		} else {
-			memset(priv->data_buffer + *off, 0, ret_size);
+			memset(priv->data_buffer + iocb->ki_pos, 0, ret_size);
 			priv->response_length -= ret_size;
-			*off += ret_size;
+			iocb->ki_pos += ret_size;
 		}
 	}
 
 out:
 	if (!priv->response_length) {
-		*off = 0;
+		iocb->ki_pos = 0;
 		del_timer_sync(&priv->user_read_timer);
 		flush_work(&priv->timeout_work);
 	}
@@ -164,10 +164,10 @@ out:
 	return ret_size;
 }
 
-ssize_t tpm_common_write(struct file *file, const char __user *buf,
-			 size_t size, loff_t *off)
+ssize_t tpm_common_write_iter(struct kiocb *iocb, struct iov_iter *from)
 {
-	struct file_priv *priv = file->private_data;
+	struct file_priv *priv = iocb->ki_filp->private_data;
+	size_t size = iov_iter_count(from);
 	int ret = 0;
 
 	if (size > TPM_BUFSIZE)
@@ -185,7 +185,7 @@ ssize_t tpm_common_write(struct file *file, const char __user *buf,
 		goto out;
 	}
 
-	if (copy_from_user(priv->data_buffer, buf, size)) {
+	if (!copy_from_iter_full(priv->data_buffer, size, from)) {
 		ret = -EFAULT;
 		goto out;
 	}
@@ -198,7 +198,7 @@ ssize_t tpm_common_write(struct file *file, const char __user *buf,
 
 	priv->response_length = 0;
 	priv->response_read = false;
-	*off = 0;
+	iocb->ki_pos = 0;
 
 	/*
 	 * If in nonblocking mode schedule an async job to send
@@ -206,7 +206,7 @@ ssize_t tpm_common_write(struct file *file, const char __user *buf,
 	 * In case of error the err code will be returned in
 	 * the subsequent read call.
 	 */
-	if (file->f_flags & O_NONBLOCK) {
+	if (iocb->ki_filp->f_flags & O_NONBLOCK) {
 		priv->command_enqueued = true;
 		queue_work(tpm_dev_wq, &priv->async_work);
 		mutex_unlock(&priv->buffer_mutex);

drivers/char/tpm/tpm-dev.c
@@ -60,8 +60,8 @@ static int tpm_release(struct inode *inode, struct file *file)
 const struct file_operations tpm_fops = {
 	.owner = THIS_MODULE,
 	.open = tpm_open,
-	.read = tpm_common_read,
-	.write = tpm_common_write,
+	.read_iter = tpm_common_read_iter,
+	.write_iter = tpm_common_write_iter,
 	.poll = tpm_common_poll,
 	.release = tpm_release,
 };

drivers/char/tpm/tpm-dev.h
@@ -23,10 +23,8 @@ struct file_priv {
 
 void tpm_common_open(struct file *file, struct tpm_chip *chip,
 		     struct file_priv *priv, struct tpm_space *space);
-ssize_t tpm_common_read(struct file *file, char __user *buf,
-			size_t size, loff_t *off);
-ssize_t tpm_common_write(struct file *file, const char __user *buf,
-			 size_t size, loff_t *off);
+ssize_t tpm_common_read_iter(struct kiocb *iocb, struct iov_iter *to);
+ssize_t tpm_common_write_iter(struct kiocb *iocb, struct iov_iter *from);
 __poll_t tpm_common_poll(struct file *file, poll_table *wait);
 
 void tpm_common_release(struct file *file, struct file_priv *priv);

drivers/char/tpm/tpmrm-dev.c
@@ -47,8 +47,8 @@ static int tpmrm_release(struct inode *inode, struct file *file)
 const struct file_operations tpmrm_fops = {
 	.owner = THIS_MODULE,
 	.open = tpmrm_open,
-	.read = tpm_common_read,
-	.write = tpm_common_write,
+	.read_iter = tpm_common_read_iter,
+	.write_iter = tpm_common_write_iter,
 	.poll = tpm_common_poll,
 	.release = tpmrm_release,
 };

linux-blktests/linux-block-trial

drivers/char/tpm/tpm-dev-common.c
@@ -127,10 +127,10 @@ void tpm_common_open(struct file *file, struct tpm_chip *chip,
 	file->private_data = priv;
 }
 
-ssize_t tpm_common_read(struct file *file, char __user *buf,
-			size_t size, loff_t *off)
+ssize_t tpm_common_read_iter(struct kiocb *iocb, struct iov_iter *to)
 {
-	struct file_priv *priv = file->private_data;
+	struct file_priv *priv = iocb->ki_filp->private_data;
+	size_t size = iov_iter_count(to);
 	ssize_t ret_size = 0;
 	int rc;
 
@@ -145,21 +145,21 @@ ssize_t tpm_common_read(struct file *file, char __user *buf,
 			goto out;
 		}
 
-		rc = copy_to_user(buf, priv->data_buffer + *off, ret_size);
+		rc = !copy_to_iter_full(priv->data_buffer + iocb->ki_pos, ret_size, to);
 		if (rc) {
 			memset(priv->data_buffer, 0, TPM_BUFSIZE);
 			priv->response_length = 0;
 			ret_size = -EFAULT;
 		} else {
-			memset(priv->data_buffer + *off, 0, ret_size);
+			memset(priv->data_buffer + iocb->ki_pos, 0, ret_size);
 			priv->response_length -= ret_size;
-			*off += ret_size;
+			iocb->ki_pos += ret_size;
 		}
 	}
 
 out:
 	if (!priv->response_length) {
-		*off = 0;
+		iocb->ki_pos = 0;
 		timer_delete_sync(&priv->user_read_timer);
 		flush_work(&priv->timeout_work);
 	}
@@ -167,10 +167,11 @@ out:
 	return ret_size;
 }
 
-ssize_t tpm_common_write(struct file *file, const char __user *buf,
-			 size_t size, loff_t *off)
+ssize_t tpm_common_write(struct kiocb *iocb, struct iov_iter *from)
 {
+	struct file *file = iocb->ki_filp;
 	struct file_priv *priv = file->private_data;
+	size_t size = iov_iter_count(from);
 	int ret = 0;
 
 	if (size > TPM_BUFSIZE)
@@ -188,7 +189,7 @@ ssize_t tpm_common_write(struct file *file, const char __user *buf,
 		goto out;
 	}
 
-	if (copy_from_user(priv->data_buffer, buf, size)) {
+	if (!copy_from_iter_full(priv->data_buffer, size, from)) {
 		ret = -EFAULT;
 		goto out;
 	}
@@ -201,7 +202,7 @@ ssize_t tpm_common_write(struct file *file, const char __user *buf,
 
 	priv->response_length = 0;
 	priv->response_read = false;
-	*off = 0;
+	iocb->ki_pos = 0;
 
 	/*
 	 * If in nonblocking mode schedule an async job to send

drivers/char/tpm/tpm-dev.c
@@ -60,8 +60,8 @@ static int tpm_release(struct inode *inode, struct file *file)
 const struct file_operations tpm_fops = {
 	.owner = THIS_MODULE,
 	.open = tpm_open,
-	.read = tpm_common_read,
-	.write = tpm_common_write,
+	.read_iter = tpm_common_read_iter,
+	.write_iter = tpm_common_write,
 	.poll = tpm_common_poll,
 	.release = tpm_release,
 };

drivers/char/tpm/tpm-dev.h
@@ -23,10 +23,8 @@ struct file_priv {
 
 void tpm_common_open(struct file *file, struct tpm_chip *chip,
 		     struct file_priv *priv, struct tpm_space *space);
-ssize_t tpm_common_read(struct file *file, char __user *buf,
-			size_t size, loff_t *off);
-ssize_t tpm_common_write(struct file *file, const char __user *buf,
-			 size_t size, loff_t *off);
+ssize_t tpm_common_read_iter(struct kiocb *iocb, struct iov_iter *to);
+ssize_t tpm_common_write(struct kiocb *iocb, struct iov_iter *from);
 __poll_t tpm_common_poll(struct file *file, poll_table *wait);
 
 void tpm_common_release(struct file *file, struct file_priv *priv);

drivers/char/tpm/tpmrm-dev.c
@@ -47,8 +47,8 @@ static int tpmrm_release(struct inode *inode, struct file *file)
 const struct file_operations tpmrm_fops = {
 	.owner = THIS_MODULE,
 	.open = tpmrm_open,
-	.read = tpm_common_read,
-	.write = tpm_common_write,
+	.read_iter = tpm_common_read_iter,
+	.write_iter = tpm_common_write,
 	.poll = tpm_common_poll,
 	.release = tpmrm_release,
 };