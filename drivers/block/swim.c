72df0f2968b28b39dda9e902a10f561ed387d1c3
Finn Thain <fthain@linux-m68k.org>
2026-08-17 11:17:10 +1000

    swim: Move swd initialization
    For better readability, initialize the swd backpointer along with the
    other floppy_state struct members. No functional change.
    
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Signed-off-by: Finn Thain <fthain@linux-m68k.org>

--- drivers/block/swim.c
+++ drivers/block/swim.c
@@ -761,6 +761,7 @@ static void swim_add_floppy(struct swim_priv *swd, enum drive_location location)
 	if (swim_track00(base))
 		goto out;
 
+	fs->swd = swd;
 	fs->location = location;
 
 	fs->type = HD_MEDIA;
@@ -849,8 +850,6 @@ static int swim_floppy_init(struct platform_device *pdev)
 			err = PTR_ERR(swd->unit[drive].disk);
 			goto exit_put_disks;
 		}
-
-		swd->unit[drive].swd = swd;
 	}
 
 	for (drive = 0; drive < swd->floppy_count; drive++) {
